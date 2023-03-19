79ff450f4ae5eb70f01916b24af63c2a40932cea
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-03-18 23:18:56 -0500

    config jest properly
    - Configure coverage
    - Mock assets files
    - Add other configs such as auto-import jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,27 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["src/index.ts", "src/.*types.*"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  },
+  moduleDirectories: ["node_modules", "src"],
   moduleNameMapper: {
-    "^[./a-zA-Z0-9_]+\\.(jpg|png|svg)$": "assets/imgs",
-    "assets/(.*)": [
-      "assets/$1",
-    ],
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy"
   },
   modulePaths: ["src"],
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest"]
+  }
 };
